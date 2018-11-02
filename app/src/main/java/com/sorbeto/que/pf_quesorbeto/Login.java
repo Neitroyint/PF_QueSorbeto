6556c991f973fd08a849e0e23f7483572e1ff076
AshleyVS <avargass633@ulacit.ed.cr>
2018-11-01 20:57:44 -0600

    Inicio de Interface de Login

---
+++ app/src/main/java/com/sorbeto/que/pf_quesorbeto/Login.java
@@ -0,0 +1,13 @@
+package com.sorbeto.que.pf_quesorbeto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login2);
+    }
+}
